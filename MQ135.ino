7a09df003679ded1a46b1fe09bcee31647fe842a
DORTHY <89404789+thisisjiye@users.noreply.github.com>
2023-02-04 21:03:14 +0900

    Add files via upload

--- MQ135.ino
+++ MQ135.ino
@@ -1,43 +1,22 @@
-#include <MQ135.h>
+#include "MQ135.h"
 
-/*  MQ135 gas sensor
-    Datasheet can be found here: https://www.olimex.com/Products/Components/Sensors/SNS-MQ135/resources/SNS-MQ135.pdf
+#define ANALOGPIN A0
+#define RZERO 206.85
 
-    Application
-    They are used in air quality control equipments for buildings/offices, are suitable for detecting of NH3, NOx, alcohol, Benzene, smoke, CO2, etc
-
-    Original creator of this library: https://github.com/GeorgK/MQ135
-*/
-
-#define PIN_MQ135 A2
-
-MQ135 mq135_sensor(PIN_MQ135);
-
-float temperature = 21.0; // Assume current temperature. Recommended to measure with DHT22
-float humidity = 25.0; // Assume current humidity. Recommended to measure with DHT22
+MQ135 gasSensor = MQ135(ANALOGPIN);
 
 void setup() {
   Serial.begin(9600);
+  delay(3000);
+  Serial.print("MQ135 RZERO Calibration value: ");
+  Serial.println(RZERO);
+
 }
 
 void loop() {
-  float rzero = mq135_sensor.getRZero();
-  float correctedRZero = mq135_sensor.getCorrectedRZero(temperature, humidity);
-  float resistance = mq135_sensor.getResistance();
-  float ppm = mq135_sensor.getPPM();
-  float correctedPPM = mq135_sensor.getCorrectedPPM(temperature, humidity);
+  float ppm = gasSensor.getPPM();
+  delay(1000);
+  Serial.print("CO2 ppm value : ");
+  Serial.println(ppm);
 
-  Serial.print("MQ135 RZero: ");
-  Serial.print(rzero);
-  Serial.print("\t Corrected RZero: ");
-  Serial.print(correctedRZero);
-  Serial.print("\t Resistance: ");
-  Serial.print(resistance);
-  Serial.print("\t PPM: ");
-  Serial.print(ppm);
-  Serial.print("\t Corrected PPM: ");
-  Serial.print(correctedPPM);
-  Serial.println("ppm");
-
-  delay(300);
 }
